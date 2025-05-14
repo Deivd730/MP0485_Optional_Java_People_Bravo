8811e454f69ae72b806320951fbfb209db1e82ea
Deivd730 <davidcastill2005@gmail.com>
2025-05-14 13:04:23 +0200

    Resolved conflict

--- People/src/main/java/view/Insert.java
+++ People/src/main/java/view/Insert.java
@@ -141,11 +141,6 @@ public class Insert extends javax.swing.JDialog {
         insert.setMaximumSize(new java.awt.Dimension(187, 33));
         insert.setMinimumSize(new java.awt.Dimension(187, 33));
         insert.setPreferredSize(new java.awt.Dimension(187, 33));
-        insert.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                insertActionPerformed(evt);
-            }
-        });
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 2;
         gridBagConstraints.gridy = 4;
